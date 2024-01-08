24efcb46c77d8aef5063fd942fdee7a00b1436ad
shyang <sunseedshenyang@gmail.com>
2024-01-08 22:18:10 +0800

    feature:开发短链接后管系统分组修改功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -2,6 +2,7 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 
 import java.util.List;
@@ -18,9 +19,15 @@ public interface GroupService extends IService<GroupDO> {
     void saveGroup(String groupName);
 
     /*
-    * 新增短链接分组
+    * 查询用户短链接分组集合
     * @return 用户短链接分组集合
     * */
     List<ShortLinkGroupRespDTO> listGroup();
+
+    /*
+     * 修改短链接分组
+     * @param requestParam 修改连接分组参数
+     * */
+    void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
 
 }
