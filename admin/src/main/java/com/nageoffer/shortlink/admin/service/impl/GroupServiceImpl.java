331592b613250fbb676b70af53fc176c3ee04f3f
shyang <sunseedshenyang@gmail.com>
2024-01-08 15:42:56 +0800

    feature:开发短链接后管分组新增功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,10 +1,13 @@
 package com.nageoffer.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
 import com.nageoffer.shortlink.admin.service.GroupService;
+import com.nageoffer.shortlink.admin.toolkit.RandomGenerator;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do{
+            gid = RandomGenerator.generateRandom();
+            if(hasGid(gid)){
+                break;
+            }
+        }while(!hasGid(gid));
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
 
+    private boolean hasGid(String gid){
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                // TODO 设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
 }
