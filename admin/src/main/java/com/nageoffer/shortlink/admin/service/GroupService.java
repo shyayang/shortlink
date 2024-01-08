2ba9b4e26c3ca2c2348ab8c0c197a69c5ea1743a
shyang <sunseedshenyang@gmail.com>
2024-01-08 11:01:16 +0800

    feature:初始化短链接分组功能代码

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -1,4 +1,10 @@
 package com.nageoffer.shortlink.admin.service;
 
-public interface GroupService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+
+/*
+* 短链接分组接口层
+* */
+public interface GroupService extends IService<GroupDO> {
 }
