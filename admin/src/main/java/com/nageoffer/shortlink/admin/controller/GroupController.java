b11481045e6c026869e26207972068cd87e27fd8
shyang <sunseedshenyang@gmail.com>
2024-01-27 19:46:19 +0800

    refactor:重构短链接后管系统基础路径

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.admin.controller;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
 import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
@@ -23,7 +24,7 @@ public class GroupController {
     /*
     * 新增短链接分组
     * */
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam){
         groupService.saveGroup(requestParam.getName());
         return Results.success();
@@ -32,7 +33,7 @@ public class GroupController {
     /*
     * 查询短链接分组集合
     * */
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> ListGroup(){
         return Results.success(groupService.listGroup());
     }
@@ -40,7 +41,7 @@ public class GroupController {
     /*
     * 修改短链接分组名称
     * */
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/api/short-link/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam){
         groupService.updateGroup(requestParam);
         return Results.success();
@@ -49,9 +50,18 @@ public class GroupController {
     /*
      * 删除短链接分组
      * */
-    @DeleteMapping("/api/short-link/v1/group")
-    public Result<Void> deleteGroup(@RequestParam String gid){
+    @DeleteMapping("/api/short-link/admin/v1/group")
+    public Result<Void> updateGroup(@RequestParam String gid){
         groupService.deleteGroup(gid);
+        return Results.success();
+    }
+
+    /*
+     * 删除短链接分组
+     * */
+    @PostMapping("/api/short-link/admin/v1/group/sort")
+    public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortReqDTO> requestParam){
+        groupService.sortGroup(requestParam);
         return Results.success();
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     * 根据用户名查询用户信息
     * */
     private final UserService userService;
-    @GetMapping("/api/short-link/v1/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
 //        UserRespDTO result = userService.getUserByUsername(username);
 //        if(result == null){
@@ -38,7 +38,7 @@ public class UserController {
     /*
      * 根据用户名查询用户无脱敏信息
      * */
-    @GetMapping("/api/short-link/v1/actual/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/actual/user/{username}")
     public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
         return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
@@ -46,7 +46,7 @@ public class UserController {
     /*
     * 查询用户名是否存在
     * */
-    @GetMapping("/api/short-link/v1/user/has-username")
+    @GetMapping("/api/short-link/admin/v1/user/has-username")
     public Result<Boolean> hasUsername(@RequestParam("username") String username){
         return Results.success(userService.hasUsername(username));
     }
@@ -54,7 +54,7 @@ public class UserController {
     /*
     * 注册用户
     * */
-    @PostMapping("/api/short-link/v1/user")
+    @PostMapping("/api/short-link/admin/v1/user")
     public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam){
         userService.register(requestParam);
         return Results.success();
@@ -63,7 +63,7 @@ public class UserController {
     /*
     * 修改用户
     * */
-    @PutMapping("/api/short-link/v1/user")
+    @PutMapping("/api/short-link/admin/v1/user")
     public Result<Void> update(@RequestBody UserUpdateReqDTO requestParam){
         userService.update(requestParam);
         return Results.success();
@@ -72,7 +72,7 @@ public class UserController {
     /*
     * 用户登录
     * */
-    @PostMapping("/api/short-link/v1/user/login")
+    @PostMapping("/api/short-link/admin/v1/user/login")
     public Result<UserLoginRespDTO> login(@RequestBody UserLoginReqDTO requestParam){
         return Results.success(userService.login(requestParam));
     }
@@ -80,7 +80,7 @@ public class UserController {
     /*
     * 检查用户是否登录
     * */
-    @GetMapping("/api/short-link/v1/user/check-login")
+    @GetMapping("/api/short-link/admin/v1/user/check-login")
     public Result<Boolean> checkLogin(@RequestParam("username") String username, @RequestParam("token") String token){
         return Results.success(userService.checkLogin(username, token));
     }
@@ -88,7 +88,7 @@ public class UserController {
     /*
     * 用户退出登录
     * */
-    @DeleteMapping("/api/short-link/v1/user/logout")
+    @DeleteMapping("/api/short-link/admin/v1/user/logout")
     public Result<Void> logout(@RequestParam("username") String username, @RequestParam("token") String token){
         userService.logout(username, token);
         return Results.success();
