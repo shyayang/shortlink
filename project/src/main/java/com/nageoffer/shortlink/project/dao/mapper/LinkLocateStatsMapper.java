a776d3b716fba9257d3a331bd1c5401efa9081c5
shyayang <shenyangwawj@163.com>
2024-02-19 16:52:47 +0800

    fix:修复短链接监控相关数据

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocateStatsMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocateStatsMapper.java
@@ -2,8 +2,13 @@ package com.nageoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.shortlink.project.dao.entity.LinkLocateStatsDO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkGroupStatsReqDTO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /*
 * 地区统计访问持久层
@@ -17,4 +22,36 @@ public interface LinkLocateStatsMapper extends BaseMapper<LinkLocateStatsDO> {
             "VALUES(#{linkLocateStats.fullShortUrl}, #{linkLocateStats.gid}, #{linkLocateStats.date}, #{linkLocateStats.cnt}, #{linkLocateStats.country}, #{linkLocateStats.province}, #{linkLocateStats.city}, #{linkLocateStats.adcode}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocateStats.cnt};")
     void shortLinkLocateStats(@Param("linkLocateStats") LinkLocateStatsDO linkLocateStatsDO);
+
+
+    /**
+     * 根据短链接获取指定日期内地区监控数据
+     */
+    @Select("SELECT " +
+            "    province, " +
+            "    SUM(cnt) AS cnt " +
+            "FROM " +
+            "    t_link_locale_stats " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    full_short_url, gid, province;")
+    List<LinkLocateStatsDO> listLocateByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据分组获取指定日期内地区监控数据
+     */
+    @Select("SELECT " +
+            "    province, " +
+            "    SUM(cnt) AS cnt " +
+            "FROM " +
+            "    t_link_locale_stats " +
+            "WHERE " +
+            "    gid = #{param.gid} " +
+            "    AND date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    gid, province;")
+    List<LinkLocateStatsDO> listLocateByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
 }
